2ba314d6fa3485dd3e73b16750a4c0e5c837e4a3
Khanh Vu <kvu9@lsu.edu>
2020-05-20 22:01:29 -0500

    web page rough draft done

--- display/models.py
+++ display/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django import forms
 
-# Create your models here.
+state = (
+    ('Baton Rouge', 'Baton Rouge, USA'),
+    ('Delhi','Delhi, India'),
+    ('Fresno','Fresno, USA'),
+    ('New York', 'New York, USA'),
+
+    ('Alabama','Alabama'),
+    ('Alaska','Alaska'),
+    ('Arizona','Arizona'),
+    ('Arkansas','Arkansas'),
+    ('California','California'),
+    ('Colorado','Colorado'),
+    ('Connecticut','Connecticut'),
+    ('Delaware','Delaware'),
+    ('Florida','Florida'),
+    ('Georgia','Georgia'),
+    ('Hawaii','Hawaii'),
+    ('Idaho','Idaho'),
+    ('Illinois','Illinois'),
+    ('Indiana','Indiana'),
+    ('Iowa','Iowa'),
+    ('Kansas','Kansas'),
+    ('Kentucky','Kentucky'),
+    ('Louisiana','Louisiana'),
+    ('Maine','Maine'),
+    ('Maryland','Maryland'),
+    ('Massachusetts','Massachusetts'),
+    ('Michigan','Michigan'),
+    ('Minnesota','Minnesota'),
+    ('Mississippi','Mississippi'),
+    ('Missouri','Missouri')
+
+
+)
+
+class spreadForm (forms.Form):
+    state = forms.CharField(widget=forms.Select(choices=state))
--- display/views.py
+++ display/views.py
@@ -1,12 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import spreadForm
+import pandas as pd
 
 def index(request):
     return render(request,'display/index.html/')
 
 def spread(request):
-    return HttpResponse("spread")
+    if request.method == 'POST':
+        form = spreadForm(request.POST or None, initial={'state': 'New York, USA'})
+        if form.is_valid():
+            state = str(form['state'].data)
+            df = pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv')
+            df_state = df[df['state'].str.contains(state)] 
+            df_state = df_state.reset_index(drop=True)
+            cases = [df_state['cases'][i] for i in df_state.index]
+            deaths = [df_state['deaths'][i] for i in df_state.index]
+            date = [df_state['date'][i] for i in df_state.index]
+
+            return render(request,'display/spread.html/', {
+                'form': form,
+                'state': state, 
+                'date': date,
+                'cases': cases,
+                'deaths': deaths,
+                })
+            #return HttpResponse(cases)
+
+    else:
+        form = spreadForm()
+        return render(request,'display/spread.html/', {'form': form})
+
 
 def deathrate(request):
     return HttpResponse("deathrate")
